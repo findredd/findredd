c987faea7371428f04a054bdaa40b65d747ea008
Abhishek Singh <a.s.2013.hts@gmail.com>
2021-04-10 14:21:35 +0530

    re-edit sqreen configs

--- findredd/wsgi.py
+++ findredd/wsgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
 import sqreen
-
-
 sqreen.start()  # Sqreen Security App starting point
+
+import os
+
 
 from django.core.wsgi import get_wsgi_application
 
